c3e6d2365c9e34792baab7013b06832a104afc3e
Cow-Mangler-5000 <nurhafizrahimi9@gmail.com>
2025-01-11 19:51:43 +0800

    1

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import Counter from "./lib/counter.svelte";
+</script>
+
+<style lang="scss">
+
+:root {
+    font-family: sans-serifs;
+    font-size: 20px;
+    text-align: center;
+}
+
+.c {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+c {
+    border: 1px solid black;
+    border-radius: 10px;
+    padding: 20px;
+    box-shadow: 1px 1px 1px 2px black;
+}
+
+</style>
+
+<div class="c">
+    <c>
+        <Counter/>
+    </c>
+</div>
---
+++ src/lib/counter.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    let count = $state(0);
+
+    const setCountIncrement = () => {
+        count++;
+    };
+
+    const setCountDecrement = () => {
+        count--;
+    };
+</script>
+
+<div class="c">
+    <button onclick="{()=>{setCountIncrement()}}">Increment</button>
+    <button onclick="{()=>{setCountDecrement()}}">Decrement</button>
+    <p>{count}</p>
+</div>
+
+<style lang="scss">
+
+@mixin btn {
+    border: 2px solid rgb(0, 0, 0);
+    background-color: beige;
+    box-shadow: 1px 1px 2px 2px rgb(192, 192, 192);
+    border-radius: 4px;
+}
+
+button {
+    @include btn();
+}
+
+</style>
